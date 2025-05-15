36d8cc229120943e9b2193a637f3d5439075ec43
Vadim <vad.constantin@gmail.com>
2025-05-15 11:45:21 +0200

    fixed typing error in pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,7 +28,7 @@ const Index:React.FC<HomePageProps> = ({ navData, homePageData }) => {
     <Wrapper>
       <NavBar navData={navData}/>
       <ModuleOneCarousel modulesOneData={homePageData.fields.modulesOne}/>
-      <ModuleTwo moduleTwoData={homePageData.fields.moduleTwo} />
+      <ModuleTwo moduleTwoData={homePageData.fields.moduleTwo as any} />
       <ModuleThree moduleThreeData={homePageData.fields.moduleThree as any}/>
       <Footer />
     </Wrapper>
